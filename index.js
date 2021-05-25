447ecabb42a030592d9d495588d69cf0d1abb91f
sngm <magnus@sungam.de>
2021-05-25 20:21:01 +0200

    Integrate Rounds feature #firsttry

--- index.js
+++ index.js
@@ -3,75 +3,129 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Some variables
-let timer_1 = 12;
-let timer_2 = 10;
+let rounds = 2;
+let roundsDone = 0;
+let timerFirst = 6;
+let timerSecond = 3;
+let timerFirstCopy = timerFirst;
+let timerSecondCopy = timerSecond;
 let startbutton = document.getElementById('startBtn');
+let startroundsbutton = document.getElementById('startRoundsBtn');
 let soundbutton = document.getElementById('soundBtn');
 
-// Write Javascript code!
 const headlineDiv = document.getElementById('headline1');
 headlineDiv.innerHTML = `<h1>Fitness Timer</h1>`;
 const counter1Div = document.getElementById('counter1');
 counter1Div.innerHTML =
-  `<span>` + `00` + `</span>` + `:` + `<span>` + timer_1 + `</span>`;
+  `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirst + `</span>`;
 const counter2Div = document.getElementById('counter2');
 counter2Div.innerHTML =
-  `<span>` + `00` + `</span>` + `:` + `<span>` + timer_2 + `</span>`;
-// const counterActionsDiv = document.getElementById('counterActions');
-// counterActionsDiv.innerHTML = `<button onclick="myTimer()">start</button>`;
+  `<span>` + `00` + `</span>` + `:` + `<span>` + timerSecond + `</span>`;
+const roundsDiv = document.getElementById('rounds');
+roundsDiv.innerHTML =
+  `<span>` + roundsDone + `</span>` + `/` + `<span>` + rounds + `</span>`;
 
-function timer_1_action() {
-  timer_1 = timer_1 - 1;
+function setTimerFirstCopy() {
+  timerFirstCopy = timerFirst;
   counter1Div.innerHTML =
-    `<span>` + `00` + `</span>` + `:` + `<span>` + timer_1 + `</span>`;
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirstCopy + `</span>`;
 }
 
-function timer_2_action() {
-  timer_2 = timer_2 - 1;
+function setTimerSecondCopy() {
+  timerSecondCopy = timerSecond;
   counter2Div.innerHTML =
-    `<span>` + `00` + `</span>` + `:` + `<span>` + timer_2 + `</span>`;
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timerSecondCopy + `</span>`;
 }
 
-function start_timers() {
+function timerFirstAction() {
+  timerFirstCopy = timerFirstCopy - 1;
+  counter1Div.innerHTML =
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirstCopy + `</span>`;
+}
+
+function timerSecondAction() {
+  timerSecondCopy = timerSecondCopy - 1;
+  counter2Div.innerHTML =
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timerSecondCopy + `</span>`;
+}
+
+function roundsAction() {
+  roundsDone = roundsDone + 1;
+  roundsDiv.innerHTML =
+    `<span>` + roundsDone + `</span>` + `/` + `<span>` + rounds + `</span>`;
+}
+
+function startTimers() {
   let timers_interval = setInterval(function() {
-    if (timer_1 > 0) {
-      timer_1_action();
-      console.log(timer_1);
+    if (timerFirstCopy > 0) {
+      timerFirstAction();
     } else {
-      console.log('else');
+      // console.log('else');
       // clearInterval(timer_1_interval);
     }
-    if (timer_1 === 0 && timer_2 > 0) {
-      timer_2_action();
+    if (timerFirstCopy === 0 && timerSecondCopy > 0) {
+      timerSecondAction();
     } else {
-      console.log('else 2');
+      // console.log('else 2');
     }
-    if (timer_1 === 0 && timer_2 === 0) {
+    if (timerFirstCopy === 0 && timerSecondCopy === 0) {
       clearInterval(timers_interval);
       console.log('Interval Timers gecleart');
+      setTimerFirstCopy();
+      setTimerSecondCopy();
     }
   }, 1000);
+}
+
+function startRounds() {
+  let timeout = (timerFirst + timerSecond) * 1000;
+  console.log('RoundsDone: ', roundsDone);
+  console.log('Rounds: ', rounds);
+  if (roundsDone === 0) {
+    console.log('if');
+    roundsAction();
+    startTimers();
+  }
+  if (roundsDone != 0 && roundsDone < rounds) {
+    console.log('else if');
+    setTimeout(function() {
+      console.log('Im Timeout drin');
+      roundsAction();
+      startTimers();
+    }, timeout);
+  }
+  if (roundsDone === rounds) {
+    console.log('Fertig');
+  }
 }
 
 startbutton.addEventListener(
   'click',
   function() {
-    start_timers();
+    startTimers();
+  },
+  false
+);
+
+startroundsbutton.addEventListener(
+  'click',
+  function() {
+    startRounds();
   },
   false
 );
 
 // Add Audio to the show
-let x = document.getElementById('timerAudio');
+// let x = document.getElementById('timerAudio');
 
-function playAudio() {
-  x.play();
-}
+// function playAudio() {
+//   x.play();
+// }
 
-soundbutton.addEventListener(
-  'click',
-  function() {
-    playAudio();
-  },
-  false
-);
+// soundbutton.addEventListener(
+//   'click',
+//   function() {
+//     playAudio();
+//   },
+//   false
+// );

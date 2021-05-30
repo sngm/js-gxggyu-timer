301af71752e226825bdcb61b771479d88415d60c
sngm <magnus@sungam.de>
2021-05-30 21:36:09 +0200

    3 files modified

--- JstTimer.js
+++ JstTimer.js
@@ -1,3 +1,6 @@
+let timerFirstCopy = '';
+let timerSecondCopy = '';
+
 class JstTimer {
   constructor(name, timeFirst, timeSecond) {
     this.name = name;
@@ -6,7 +9,63 @@ class JstTimer {
     this.timeSecond = timeSecond;
   }
 
+  /*   _setTimerFirstCopy() {
+    timerFirstCopy = this.timeFirst;
+    counter1Div.innerHTML =
+      `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirstCopy + `</span>`;
+  }
+  _setTimerSecondCopy() {
+    timerSecondCopy = this.timeSecond;
+    counter2Div.innerHTML =
+      `<span>` +
+      `00` +
+      `</span>` +
+      `:` +
+      `<span>` +
+      timerSecondCopy +
+      `</span>`;
+  }
+
+  _timerFirstAction() {
+    timerFirstCopy = timerFirstCopy - 1;
+    counter1Div.innerHTML =
+      `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirstCopy + `</span>`;
+  }
+  _timerSecondAction() {
+    timerSecondCopy = timerSecondCopy - 1;
+    counter2Div.innerHTML =
+      `<span>` +
+      `00` +
+      `</span>` +
+      `:` +
+      `<span>` +
+      timerSecondCopy +
+      `</span>`;
+  }
+
   // TODO: Timer Start Methode einbauen
+  start() {
+    this._setTimerFirstCopy();
+    this._setTimerSecondCopy();
+    let timersInterval = setInterval(function() {
+      if (timerFirstCopy > 0) {
+        timerFirstAction();
+      } else {
+        // console.log('else');
+      }
+      if (timerFirstCopy === 0 && timerSecondCopy > 0) {
+        timerSecondAction();
+      } else {
+        // console.log('else 2');
+      }
+      if (timerFirstCopy === 0 && timerSecondCopy === 0) {
+        clearInterval(timersInterval);
+        console.log('Interval Timers gecleart');
+        setTimerFirstCopy();
+        setTimerSecondCopy();
+      }
+    }, 1000);
+  } */
 }
 
 export { JstTimer };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 // Imports
 import { JstUnit as Unit } from './JstUnit.js';
 import { JstActivity as Activity } from './JstActivity.js';
@@ -6,41 +7,39 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Some variables
-let rounds = 20;
-let roundsDone = 0;
-let timerFirst = 30;
-let timerSecond = 20;
-let timerFirstCopy = timerFirst;
-let timerSecondCopy = timerSecond;
-let startbutton = document.getElementById('startBtn');
-let startroundsbutton = document.getElementById('startRoundsBtn');
-let soundbutton = document.getElementById('soundBtn');
+// let rounds = 20;
+// let roundsDone = 0;
+// let timerFirst = 30;
+// let timerSecond = 20;
+let timeFirst;
+let timeSecond;
+// let startroundsbutton = document.getElementById('startRoundsBtn');
+// let soundbutton = document.getElementById('soundBtn');
 
-let myFirstActivity = new Activity('Liegestütz', 'myTimer');
-console.log(myFirstActivity);
-let mySecondActivity = new Activity('Superman', 'myTimer');
-console.log(mySecondActivity);
-
-let myUnit = new Unit('My Unit', [myFirstActivity, mySecondActivity]);
-console.log(myUnit);
-
-let myTimer = new Timer('My Timer', 10000, 5000);
+// let myFirstActivity = new Activity('Liegestütz', 'myTimer');
+// console.log(myFirstActivity);
+// let mySecondActivity = new Activity('Superman', 'myTimer');
+// console.log(mySecondActivity);
+// let myUnit = new Unit('My Unit', [myFirstActivity, mySecondActivity]);
+// console.log(myUnit);
+let myTimer = new Timer('My First Timer', 5, 5);
 console.log(myTimer);
 
 const app = document.getElementById('app');
-const appUnit = document.getElementById('unit');
-appUnit.innerHTML =
-  '<p><strong>Unit</strong></p>' +
+// const appUnit = document.getElementById('unit');
+// appUnit.innerHTML =
+/* '<p><strong>Unit</strong></p>' +
   '<p>' +
   myUnit.name +
   '</p>' +
   '<p>' +
   myUnit.activities +
-  '</p>';
+  '</p>' +
+  '<button id="startUnitBtn">Start Unit</button>'; */
 
-const appActivities = document.getElementById('activities');
-appActivities.innerHTML =
-  '<p><strong>Activities</strong></p>' +
+// const appActivities = document.getElementById('activities');
+// appActivities.innerHTML =
+/* '<p><strong>Activities</strong></p>' +
   '<p>' +
   myFirstActivity.name +
   '</p>' +
@@ -52,7 +51,7 @@ appActivities.innerHTML =
   '</p>' +
   '<p>' +
   mySecondActivity.timer +
-  '</p>';
+  '</p>'; */
 
 const appTimer = document.getElementById('timer');
 appTimer.innerHTML =
@@ -65,74 +64,89 @@ appTimer.innerHTML =
   ' ms</p>' +
   '<p>' +
   myTimer.timeSecond +
-  ' ms</p>';
+  ' ms</p>' +
+  '<button id="startTimerBtn">Start Timer</button>';
 
-/* const body = document.getElementsByTagName('body')[0];
-body.classList.add('bg-primary');
-const headlineDiv = document.getElementById('headline1');
-headlineDiv.innerHTML = `<h1>Fitness Timer</h1>`;
+let startTimerBtn = document.getElementById('startTimerBtn');
+
+startTimerBtn.addEventListener(
+  'click',
+  function() {
+    startTimers();
+  },
+  false
+);
+
+// const body = document.getElementsByTagName('body')[0];
+// body.classList.add('bg-primary');
+// const headlineDiv = document.getElementById('headline1');
+// headlineDiv.innerHTML = `<h1>Fitness Timer</h1>`;
 const counter1Div = document.getElementById('counter1');
 counter1Div.innerHTML =
-  `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirst + `</span>`;
+  `<span>` + `00` + `</span>` + `:` + `<span>` + timeFirst + `</span>`;
 const counter2Div = document.getElementById('counter2');
 counter2Div.innerHTML =
-  `<span>` + `00` + `</span>` + `:` + `<span>` + timerSecond + `</span>`;
-const roundsDiv = document.getElementById('rounds');
-roundsDiv.innerHTML =
-  `<span>` + roundsDone + `</span>` + `/` + `<span>` + rounds + `</span>`;
+  `<span>` + `00` + `</span>` + `:` + `<span>` + timeSecond + `</span>`;
+// const roundsDiv = document.getElementById('rounds');
+// roundsDiv.innerHTML =
+// `<span>` + roundsDone + `</span>` + `/` + `<span>` + rounds + `</span>`;
 
-function setTimerFirstCopy() {
-  timerFirstCopy = timerFirst;
+function setTimeFirst() {
+  timeFirst = myTimer.timeFirst;
   counter1Div.innerHTML =
-    `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirstCopy + `</span>`;
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timeFirst + `</span>`;
 }
 
-function setTimerSecondCopy() {
-  timerSecondCopy = timerSecond;
+setTimeFirst();
+
+function setTimeSecond() {
+  timeSecond = myTimer.timeSecond;
   counter2Div.innerHTML =
-    `<span>` + `00` + `</span>` + `:` + `<span>` + timerSecondCopy + `</span>`;
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timeSecond + `</span>`;
 }
 
-function timerFirstAction() {
-  timerFirstCopy = timerFirstCopy - 1;
+setTimeSecond();
+
+function timeFirstAction() {
+  timeFirst = timeFirst - 1;
   counter1Div.innerHTML =
-    `<span>` + `00` + `</span>` + `:` + `<span>` + timerFirstCopy + `</span>`;
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timeFirst + `</span>`;
 }
 
-function timerSecondAction() {
-  timerSecondCopy = timerSecondCopy - 1;
+function timeSecondAction() {
+  timeSecond = timeSecond - 1;
   counter2Div.innerHTML =
-    `<span>` + `00` + `</span>` + `:` + `<span>` + timerSecondCopy + `</span>`;
-}
-
-function roundsAction() {
-  roundsDone = roundsDone + 1;
-  roundsDiv.innerHTML =
-    `<span>` + roundsDone + `</span>` + `/` + `<span>` + rounds + `</span>`;
+    `<span>` + `00` + `</span>` + `:` + `<span>` + timeSecond + `</span>`;
 }
 
 function startTimers() {
   let timersInterval = setInterval(function() {
-    if (timerFirstCopy > 0) {
-      timerFirstAction();
+    if (timeFirst > 0) {
+      timeFirstAction();
     } else {
       // console.log('else');
     }
-    if (timerFirstCopy === 0 && timerSecondCopy > 0) {
-      timerSecondAction();
+    if (timeFirst === 0 && timeSecond > 0) {
+      timeSecondAction();
     } else {
       // console.log('else 2');
     }
-    if (timerFirstCopy === 0 && timerSecondCopy === 0) {
+    if (timeFirst === 0 && timeSecond === 0) {
       clearInterval(timersInterval);
       console.log('Interval Timers gecleart');
-      setTimerFirstCopy();
-      setTimerSecondCopy();
+      setTimeFirst();
+      setTimeSecond();
     }
   }, 1000);
 }
 
-function startRounds() {
+/* function roundsAction() {
+  roundsDone = roundsDone + 1;
+  roundsDiv.innerHTML =
+    `<span>` + roundsDone + `</span>` + `/` + `<span>` + rounds + `</span>`;
+} */
+
+/* function startRounds() {
   let timeout = (timerFirst + timerSecond) * 1000;
 
   for (let i = 0; i < rounds; i++) {
@@ -144,17 +158,9 @@ function startRounds() {
     if (i === rounds - 1) {
     }
   }
-}
+} */
 
-startbutton.addEventListener(
-  'click',
-  function() {
-    startTimers();
-  },
-  false
-);
-
-startroundsbutton.addEventListener(
+/* startroundsbutton.addEventListener(
   'click',
   function() {
     startRounds();
